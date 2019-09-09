627e7a8bd151b94f42154ace16060c7a89f9eb8a
Francisco Javier López <fcojavierlpz91@gmail.com>
2019-09-09 00:24:31 -0500

    Config webpack & Sass

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: './js/bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          // fallback to style-loader in development
+          /*process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,*/
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // 'postcss-loader',
+          'resolve-url-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: './css/style.css'
+    }),
+  ],
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript 
+  // minifying and other thing so let's set mode to development
+  mode: 'development'
+};
