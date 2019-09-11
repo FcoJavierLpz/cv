4f5a9f27a3e1ff3a13ec98f23d48a2380b537ad0
Francisco Javier López <fcojavierlpz91@gmail.com>
2019-09-11 15:11:41 -0500

    fix reload webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,38 @@
 const path = require('path'),
-  MiniCssExtractPlugin = require('mini-css-extract-plugin');
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/js/index.js',
   output: {
     filename: './js/bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
   },
   module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
+    rules: [{
+      test: /\.s[ac]ss$/i,
+      use: [{
           // Creates `style` nodes from JS strings
           // fallback to style-loader in development
-          /*process.env.NODE_ENV !== 'production'
-            ? 'style-loader'
-            : MiniCssExtractPlugin.loader,*/
-          MiniCssExtractPlugin.loader,
+          loader: process.env.NODE_ENV !== 'production' ?
+            'style-loader' :
+            MiniCssExtractPlugin.loader,
+        },
+        {
           // Translates CSS into CommonJS
-          'css-loader',
-          // 'postcss-loader',
-          'resolve-url-loader',
+          loader: 'css-loader'
+        },
+        {
           // Compiles Sass to CSS
-          'sass-loader',
-        ]
-      }
-    ]
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true
+          }
+        }
+      ]
+    }]
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -34,10 +40,18 @@ module.exports = {
       // both options are optional
       filename: './css/style.css'
     }),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
   ],
+  devServer: {
+    compress: true,
+    port: 8080,
+    inline: true
+  },
   // Default mode for Webpack is production.
   // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript 
+  // on final bundle. For now we don't need production's JavaScript
   // minifying and other thing so let's set mode to development
   // mode: 'development'
 };
