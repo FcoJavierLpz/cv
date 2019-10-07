607e2dfbd9bf0faa9a4eadfaa5acbf9cd385fbb6
Francisco Javier López <fcojavierlpz91@gmail.com>
2019-10-07 01:15:37 -0500

    Adding skills transitions

--- src/js/app.ts
+++ src/js/app.ts
@@ -4,7 +4,27 @@ const removeFadeOut = ( el:any, speed:number ) => {
     el.style.opacity = 0;
     setTimeout(() => {
         el.parentNode.removeChild(el);
+        document.querySelector(".container").removeAttribute("style");
     }, speed);
+}
+var lang = {
+    "html": "100%",
+    "css": "90%",
+    "javascript": "70%",
+    "php": "55%",
+    "angular": "65%",
+    "node": "75%",
+    "react": "85%"
+  };
+  
+  var multiply = 4;
+  
+for (const [index, value] of Object.entries(lang)) {
+    var delay = 700;
+    setTimeout(function () {
+        document.querySelector('#' + index + '-pourcent').innerHTML = value;
+    }, delay * multiply);
+    multiply++;
 }
 
 export const loading = () => {
