2717a22d50c792ee91631b22a66e7339d8cd1dda
Francisco J. López <fcojavierlpz91@gmail.com>
2020-01-19 12:36:19 -0600

    In progress fix src

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,6 @@
 import '../sass/style.scss';
 import { loading } from './app'
-import Profile from '/src/images/pixel_paco.png';
+import Profile from '../images/pixel_paco.png';
 
 loading();
    // Add the image to our existing div.
