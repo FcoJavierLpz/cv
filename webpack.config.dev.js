3504ed2871422451a0625f843aeca19d7112f5cc
flopez <flopez@linko.mx>
2020-04-05 21:37:15 -0500

    refactor(webpack)

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,79 +1,77 @@
-const path = require('path'),
-  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
-  HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path"),
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: './src/js/index.ts',
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/js/index.ts",
   output: {
-    filename: './js/bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "./js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: [{
+        use: [
+          {
             // Creates `style` nodes from JS strings
             // fallback to style-loader in development
-            loader: 'style-loader'
+            loader: "style-loader",
           },
           {
             // Translates CSS into CommonJS
-            loader: 'css-loader'
+            loader: "css-loader",
           },
           {
             // Compiles Sass to CSS
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-        loader: 'url-loader?limit=100000'
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-            {
-            loader: 'file-loader',
+              sourceMap: true,
             },
+          },
         ],
       },
       {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-              'file-loader',
-          ],
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
-    ]
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: "url-loader?limit=100000",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
+    extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
   },
   plugins: [
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: './css/style.css'
+      filename: "./css/style.css",
     }),
     new HtmlWebpackPlugin({
-      template: './index.html'
-    })
+      template: "./index.html",
+    }),
   ],
   devServer: {
     compress: true,
     port: 8888,
-    inline: true
+    inline: true,
   },
   // Default mode for Webpack is production.
   // Depending on mode Webpack will apply different things
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,83 +1,76 @@
-const path = require('path'),
-    MiniCssExtractPlugin = require('mini-css-extract-plugin'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path"),
+  MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/js/index.ts',
-    output: {
-        filename: './js/bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [{
-                        // Creates `style` nodes from JS strings
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        // Translates CSS into CommonJS
-                        loader: 'css-loader'
-                    },
-                    {
-                        // Compiles Sass to CSS
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
+  mode: "production",
+  entry: "./src/js/index.ts",
+  output: {
+    filename: "./js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            // Creates `style` nodes from JS strings
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // Translates CSS into CommonJS
+            loader: "css-loader",
+          },
+          {
+            // Compiles Sass to CSS
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
             },
-            {
-                test: /\.ts?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-                loader: 'url-loader?limit=100000'
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[path][name].[ext]',
-                        outputPath: 'src/images',
-                        publicPath: 'src/images'
-                    }
-                    },
-                ],
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
-        ]
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".css", ".scss"]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: './css/style.css'
-        }),
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new CleanWebpackPlugin()
-    ]
-    // Default mode for Webpack is production.
-    // Depending on mode Webpack will apply different things
-    // on final bundle. For now we don't need production's JavaScript
-    // minifying and other thing so let's set mode to development
-    // mode: 'development'
+          },
+        ],
+      },
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: "url-loader?limit=100000",
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "./css/style.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on final bundle. For now we don't need production's JavaScript
+  // minifying and other thing so let's set mode to development
+  // mode: 'development'
 };
